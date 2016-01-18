4c706a3887a1ac339db118c9b16e7794b0e800b9
Madhan Ganesh <madhanganesh@karma.local>
2016-01-19 04:33:18 +0530

    added golang and node files

--- phonebook/golang/server.go
+++ phonebook/golang/server.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+	"html"
+	"labix.org/v2/mgo"
+	"log"
+	"net/http"
 )
 
+var collection *mgo.Collection
+
+func init() {
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+	//defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	collection = session.DB("test").C("person")
+}
+
+type Person struct {
+	Id        bson.ObjectId `json:"id" bson:"_id"`
+	FirstName string        `json:"firstname" bson:"firstName"`
+	LastName  string        `json:"lastname" bson:"lastName"`
+	Phone     string        `json:"phone" bson:"phone"`
+	Email     string        `json:"email" bson":"email"`
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
+}
+
+func PersonHandler(w http.ResponseWriter, r *http.Request) {
+	var results []Person
+	err := collection.Find(nil).All(&results)
+	if err != nil {
+		panic(err)
+	}
+	json.NewEncoder(w).Encode(results)
+}
+
 func main() {
-	fmt.Println("hello, go!")
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/people", PersonHandler)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- phonebook/nodejs/app.js
+++ phonebook/nodejs/app.js
@@ -1,1 +1,28 @@
-console.log('hllo, nodejs');
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080; 
+
+var uri = 'mongodb://localhost:27017/test';
+mongoose.connect(uri);
+var PersonSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    phone: String,
+    email: String
+});
+var PeopleModel = mongoose.model('People', PersonSchema, 'person');
+
+app.get('/people', (req, res) => {
+    PeopleModel.find((err, people) => {
+        if (err) {
+            es.send(err);
+        }
+
+        res.json(people);
+    });
+});
+
+app.listen(port, () => {
+    console.log('listening in port 8080..');
+});
